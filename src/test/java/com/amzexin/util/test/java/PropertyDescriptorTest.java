6637ddbaee98031d69303ae529e4a952b5efa676
lizexin <zexin_321@163.com>
2022-07-18 14:41:23 +0800

    PropertyDescriptorTest.java

---
+++ src/test/java/com/amzexin/util/test/java/PropertyDescriptorTest.java
@@ -0,0 +1,28 @@
+package com.amzexin.util.test.java;
+
+import com.amzexin.util.http.HttpResult;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Description: PropertyDescriptorTest
+ *
+ * @author Lizexin
+ * @date 2022-07-18 14:38
+ */
+@Slf4j
+public class PropertyDescriptorTest {
+
+    @Test
+    public void run() throws IntrospectionException, InvocationTargetException, IllegalAccessException {
+        Object obj = new HttpResult();
+        PropertyDescriptor propertyDescriptor = new PropertyDescriptor("code", obj.getClass());
+        Object propertyValue = propertyDescriptor.getReadMethod().invoke(obj);
+        System.out.println(propertyValue);
+    }
+
+}
