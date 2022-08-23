81a97af51ab96083e623be98e5da1c97098a348f
zexin <amzexin@163.com>
2022-08-23 18:35:36 +0800

    maven 打jar包

--- commons-test/src/main/java/io/github/amzexin/commons/test/gc/GCTest.java
+++ commons-test/src/main/java/io/github/amzexin/commons/test/gc/GCTest.java
@@ -6,16 +6,17 @@ import java.util.Arrays;
  * Description: GCTest
  * 测试方法：
  * 1. 通过开发工具打一个jar
- * 2. java -Xms4m -Xmx32m -jar commons-test.jar
+ * 2. nohup java -Xms4m -Xmx32m -XX:+HeapDumpOnOutOfMemoryError -jar commons-test.jar > gxtext.log &
  * <p>
  * JVM参数说明：
  * -Xms 最小堆内存, 默认是服务器的1/64
  * -Xmx 最大堆内存, 默认是服务器的1/4
+ * -XX:+ExitOnOutOfMemoryError OOM后，应用程序直接退出
  * -XX:+HeapDumpOnOutOfMemoryError 在OOM时，产生进程的dump文件
  * -XX:HeapDumpPath 指定dump文件的生成路径，若不指定则默认在进程的工作目录生成dump文件；还可以指定dump文件的名称
  * -Xloggc 指定gc日志文件路径（包括文件名）
  * -XX:OnOutOfMemoryError 可以指定shell命令、shell脚本、其他脚本等，在OOM时执行一些额外的操作（实践证明，是先产生dump文件，再执行配置）
- * -XX:+ExitOnOutOfMemoryError，OOM后，应用程序直接退出
+ *
  *
  * @author Lizexin
  * @date 2022-08-22 14:42
