b5a23265792a3eb77a2a836a2e6c7fc554f51756
lizexin <amzexin@163.com>
2022-10-12 21:04:54 +0800

    char强转int即为ascii

---
+++ commons-test/src/test/java/io/github/amzexin/commons/test/jdk/java/JdkTest.java
@@ -0,0 +1,17 @@
+package io.github.amzexin.commons.test.jdk.java;
+
+import org.junit.Test;
+
+/**
+ * TestTest
+ *
+ * @author zexin
+ */
+public class JdkTest {
+
+    @Test
+    public void test20221012_2102() {
+        char c = '0';
+        System.out.println((int) c);
+    }
+}
